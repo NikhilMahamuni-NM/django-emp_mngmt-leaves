3e79333eeaa3d7446a54d6905b8ab4b4e5634575
NikhilMahamuni-NM <nikhil.mahamuni01@gmail.com>
2020-09-11 11:52:39 +0530

    first commit

--- leave/admin.py
+++ leave/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import Profile, Leave
+from import_export.admin import ImportExportModelAdmin
 
+#Customizing Contact Model in Admin Site
+class LeaveAdmin(ImportExportModelAdmin):
+    list_display = ('full_name', 'leaves_avai', 'start_date', 'end_date', 'total')
+    list_display_links = ('full_name',)
+    #list_editable = ('info',)
+    list_per_page = 10
+    search_fields = ('full_name',)
+    list_filter = ('approval',)
 # Register your models here.
 admin.site.register(Profile)
-admin.site.register(Leave)
+admin.site.register(Leave, LeaveAdmin)
